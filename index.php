0982c2779215bef8e29d9dbd18bba74c694cbf16
mikalaki <mikalakis@hotmail.gr>
2021-01-12 01:30:59 +0200

    Fixed Bottom section

--- index.php
+++ index.php
@@ -182,88 +182,23 @@
       </div>
     </section><!-- End My & Family Section -->
 
-    <!-- ======= Features Section ======= -->
-    <!-- <section id="features" class="features">
-      <div class="container">
-
-        <div class="row">
-          <div class="col-lg-4 col-md-6 icon-box">
-            <div class="icon"><i class="icofont-computer"></i></div>
-            <h4 class="title"><a href="">Lorem Ipsum</a></h4>
-            <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
-          </div>
-          <div class="col-lg-4 col-md-6 icon-box">
-            <div class="icon"><i class="icofont-chart-bar-graph"></i></div>
-            <h4 class="title"><a href="">Dolor Sitema</a></h4>
-            <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
-          </div>
-          <div class="col-lg-4 col-md-6 icon-box">
-            <div class="icon"><i class="icofont-earth"></i></div>
-            <h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
-            <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-          </div>
-          <div class="col-lg-4 col-md-6 icon-box">
-            <div class="icon"><i class="icofont-image"></i></div>
-            <h4 class="title"><a href="">Magni Dolores</a></h4>
-            <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-          </div>
-          <div class="col-lg-4 col-md-6 icon-box">
-            <div class="icon"><i class="icofont-settings"></i></div>
-            <h4 class="title"><a href="">Nemo Enim</a></h4>
-            <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
-          </div>
-          <div class="col-lg-4 col-md-6 icon-box">
-            <div class="icon"><i class="icofont-tasks-alt"></i></div>
-            <h4 class="title"><a href="">Eiusmod Tempor</a></h4>
-            <p class="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
-          </div>
-        </div>
-
-      </div>
-    </section> -->
-    <!-- End Features Section -->
 
     <!-- ======= Recent Photos Section ======= -->
     <section id="log-in" class="recent-photos page-section portfolio">
-
-      <div class="container">
-
-              <div class="section-title">
-                <h2 class="noncapitalize-h2-title">Log in as:</h2>
-
-              </div>
-              <div class="container">
-
-                <hr class="mt-2 mb-5">
-
-                <div class="row text-center text-lg-left">
-
-                  <div class="col-lg-3 col-md-4 col-6">
-                    <a href="login.php?type=doctor" class="d-block mb-4 h-100">
-                          <img  class="img-fluid img-thumbnail imageNoBorder" src="assets/img/stethoscope.png" alt="">
-                        </a>
-                  </div>
-                  <div class="col-lg-3 col-md-4 col-6">
-                    <a  href="login.php?type=patient" class="d-block mb-4 h-100">
-                          <img   class="img-fluid img-thumbnail imageNoBorder" src="assets/img/old-people.png" alt="">
-                        </a>
-                  </div>
-                  <div class="col-lg-3 col-md-4 col-6">
-                    <a  href="login.php?type=pharmacy" class="d-block mb-4 h-100">
-                          <img  class="img-fluid img-thumbnail imageNoBorder" src="assets/img/cross.png" alt="">
-                        </a>
-                  </div>
-                  <div class="col-lg-3 col-md-4 col-6">
-                    <a  href="login.php?type=company" class="d-block mb-4 h-100">
-                          <img  class="img-fluid img-thumbnail imageNoBorder" src="assets/img/enterprise.png" alt="">
-                        </a>
-                  </div>
+    <?php
+      if (!isset($_SESSION['loggedin']) ) {
+        require("login_as.php");
+      }
+      else{
+        echo "<div class=\"section-title\">
+                <h2 class=\"noncapitalize-h2-title\">Logged in as ".$_SESSION['usertype']."</h2>
+                <a href=".$_SESSION['usertype'] . " ><h4>Go to Application  <i class=\"fas fa-sign-in-alt\"></i> </h4> </a>
+              </div>";
+      }
 
 
-                </div>
+      ?>
 
-              </div>
-            </div>
 
 
     </section><!-- End Recent Photos Section -->
---
+++ login_as.php
@@ -0,0 +1,43 @@
+
+<div class="container">
+
+        <div class="section-title">
+          <h2 class="noncapitalize-h2-title">Log in as:</h2>
+
+        </div>
+        <div class="container">
+
+          <hr class="mt-2 mb-5">
+
+          <div class="row text-center text-lg-left">
+
+            <div class="col-lg-3  col-6">
+              <a href="login.php?type=doctor" class="d-block mb-4 h-100">
+                    <img  class="img-fluid img-thumbnail imageNoBorder" src="assets/img/stethoscope.png" alt="">
+                    <h4 style="text-align:center">Doctor</h4>
+                  </a>
+            </div>
+            <div class="col-lg-3  col-6">
+              <a  href="login.php?type=patient" class="d-block mb-4 h-100">
+                    <img   class="img-fluid img-thumbnail imageNoBorder" src="assets/img/old-people.png" alt="">
+                    <h4 style="text-align:center">Patient</h4>
+                  </a>
+            </div>
+            <div class="col-lg-3  col-6">
+              <a  href="login.php?type=pharmacy" class="d-block mb-4 h-100">
+                    <img  class="img-fluid img-thumbnail imageNoBorder" src="assets/img/cross.png" alt="">
+                    <h4 style="text-align:center">Pharmacy</h4>
+                  </a>
+            </div>
+            <div class="col-lg-3  col-6">
+              <a  href="login.php?type=company" class="d-block mb-4 h-100">
+                    <img  class="img-fluid img-thumbnail imageNoBorder" src="assets/img/enterprise.png" alt="">
+                    <h4 style="text-align:center">Company</h4>
+                  </a>
+            </div>
+
+
+          </div>
+
+        </div>
+      </div>
